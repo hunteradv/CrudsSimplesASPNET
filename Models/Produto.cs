4616c540ba58c56c09d48f1a89a9f1d9e6b62b4c
hunteradv <gustavopaulody@gmail.com>
2021-11-26 13:00:09 -0300

    Melhorando descrições e exceções

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,9 +1,15 @@
-﻿namespace CursoMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CursoMVC.Models
 {
     public class Produto
     {
         public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo descrição é obrigatório")]
         public string Descricao { get; set; }
+
+        [Range (1,10, ErrorMessage = "O campo quantidade precisa ser entre 1 e 10")]
         public int Quantidade { get; set; }
         public int CategoriaId { get; set; }
         public Categoria Categoria { get; set; }
